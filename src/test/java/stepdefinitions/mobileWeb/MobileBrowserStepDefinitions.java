5663e717d134b9fb829a6dfb8061e6c9a474b14e
Nevzat <nevzatcelik151907@gmail.com>
2024-03-29 20:32:45 +0100

    fifth cucumber

--- src/test/java/stepdefinitions/mobileWeb/MobileBrowserStepDefinitions.java
+++ src/test/java/stepdefinitions/mobileWeb/MobileBrowserStepDefinitions.java
@@ -1,14 +1,37 @@
 package stepdefinitions.mobileWeb;
 
 import io.cucumber.java.en.Given;
+import org.junit.Assert;
+import pages.ObiletPage;
 import utils.BrowserDriver;
-import utils.Driver;
 
 public class MobileBrowserStepDefinitions {
 
-
+   ObiletPage obiletPage=new ObiletPage();
     @Given("Kullanici {string} adresine gider")
     public void kullanici_adresine_gider(String url) {
         BrowserDriver.getBrowserDriver().get(url);
     }
+
+    @Given("dil olarak turkce secilir")
+    public void dil_olarak_turkce_secilir() {
+     obiletPage.hamburgerMenu.click();
+     obiletPage.languageSelect.click();
+     obiletPage.turkishLanguage.click();
+    }
+    @Given("para birimi olarak tl secilir")
+    public void para_birimi_olarak_tl_secilir() {
+     obiletPage.hamburgerMenu.click();
+     obiletPage.currencySelect.click();
+     obiletPage.turkishLira.click();
+    }
+    @Given("otobus bileti bul a tiklanir")
+    public void otobus_bileti_bul_a_tiklanir() {
+        obiletPage.searchButton.click();
+    }
+    @Given("gelen bilet fiyatlarinin {string} oldugu dogrulanir")
+    public void gelen_bilet_fiyatlarinin_tl_oldugu_dogrulanir(String expected) {
+     Assert.assertTrue(obiletPage.priceText.getText().contains(expected));
+    }
+
 }
